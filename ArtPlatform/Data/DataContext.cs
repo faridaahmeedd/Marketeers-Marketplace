8096b8ef31b9b4706d48481444eee9f09f9df462
faridaahmeedd <faridaahmedmohamed11@gmail.com>
2024-09-01 13:30:06 +0300

    first commit

---
+++ ArtPlatform/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using ArtPlatform.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtPlatform.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext()
+		{
+		}
+
+		public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+		public DbSet<Artist> Artists { get; set; }
+
+		//protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		//{
+		//	base.OnConfiguring(optionsBuilder);
+		//}
+	}
+}
---
+++ ArtPlatform/Models/Artist.cs
@@ -0,0 +1,9 @@
+﻿namespace ArtPlatform.Models
+{
+	public class Artist
+	{
+        public int Id { get; set; }
+		public required string FName { get; set; }
+		public required string LName { get; set; }
+    }
+}
